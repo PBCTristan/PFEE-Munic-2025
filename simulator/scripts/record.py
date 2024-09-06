9bcfc61e28cf8dd9c9e9a05dadaa8c7bdc863bde
Garice Morin <garice.morin@epita.fr>
2024-09-06 14:45:39 +0200

    fix: adding hit information
    Signed-off-by: Garice Morin <garice.morin@epita.fr>

--- simulator/scripts/record.py
+++ simulator/scripts/record.py
@@ -1,19 +1,20 @@
 class Record:
-  def __init__(self, type):
-    self.is_crash = False
-    self.type = type
-    self.datas = []
-    self.actions = []
+    def __init__(self, type):
+        self.is_crash = False
+        self.type = type
+        self.datas = []
+        self.actions = []
 
-  def add_data(self, data, action):
-    self.datas.append(data)
-    self.actions.append(action)
+    def add_data(self, data, action):
+        self.datas.append(data)
+        if data["hit"] != "none":
+            self.is_crash = True
+        self.actions.append(action)
 
-  def to_json(self):
-    return {
-      'iscrash': self.is_crash,
-      'type': self.type,
-      'data': self.datas,
-      'actions': self.actions
-    }
-  
+    def to_json(self):
+        return {
+            "iscrash": self.is_crash,
+            "type": self.type,
+            "data": self.datas,
+            "actions": self.actions,
+        }
