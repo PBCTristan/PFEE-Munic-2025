cdaa6890d2b358310e246c765f63436fa48a28a1
Garice Morin <garice.morin@epita.fr>
2024-09-06 15:35:03 +0200

    feat: random movement
    Signed-off-by: Garice Morin <garice.morin@epita.fr>

--- simulator/scripts/run.py
+++ simulator/scripts/run.py
@@ -19,12 +19,11 @@ stdout_handler.setLevel(logging.INFO)
 logger.addHandler(stdout_handler)
 
 
-def setup_env():
-    PATH_TO_APP = "../DonkeySimLinux"
-    exe_path = f"{PATH_TO_APP}/donkey_sim.x86_64"
+def setup_env(env_name):
+    exe_path = "../DonkeySimLinux/donkey_sim.x86_64"
     port = 9091
-    conf = {"exe_path": exe_path, "port": port}
-    return gym.make("donkey-generated-track-v0", conf=conf)
+    conf = {"exe_path": exe_path, "port": port, "max_cte": sys.maxsize}
+    return gym.make(env_name, conf=conf)
 
 
 def play_simulation(env, options):
@@ -34,7 +33,7 @@ def play_simulation(env, options):
         case "turn":
             pass
         case "random":
-            pass
+            play_simulation_random(env, options)
         case "fixed":
             pass
 
@@ -61,57 +60,100 @@ def play_simulation_straight(env, options):
     env.close()
 
 
-parser = argparse.ArgumentParser(
-    description="Python utility to generate data from donkey car."
-)
+def play_simulation_random(env, options):
+    for i in range(options.number):
+        # Reset the environment
+        obv = env.reset()
+        logger.info(f"Running sim number {i}")
+        with open(f"generated_data/{options.type}_{i}.json", "w+") as f:
+            r = record.Record(options.type)
+            for _ in range(options.frames):
+                # Select an action
+                action = env.action_space.sample()
 
-parser.add_argument("number", type=int, help="The number of files to generate.")
-parser.add_argument(
-    "type",
-    choices=["straight", "turn", "random", "fixed"],
-    help="How are the tests conducted.",
-)
-parser.add_argument(
-    "--frames",
-    "-f",
-    required=False,
-    default=100,
-    type=int,
-    help="How many frames to record, better if between 100 and 10000. Default: 100.",
-)
-parser.add_argument(
-    "--logfile",
-    type=str,
-    required=False,
-    dest="log",
-    help="A file in which to save the logs.",
-)
-parser.add_argument(
-    "--autologfile",
-    action="store_true",
-    dest="autolog",
-    help="Create the logfiles automatically at a default location. Cannot be used in conjunction with --logfile.",
-)
-args = parser.parse_args()
+                # execute the action
+                obv, reward, done, info = env.step(action)
+                r.add_data(info, action.tolist())
+            json.dump(r.to_json(), f)
+    env.close()
 
-if args.log:
-    if args.autolog:
-        raise AssertionError("Cannot use autologfile and logfile at the same time!")
-    fileHandler = logging.FileHandler(args.log, mode="w+")
-    fileHandler.setLevel(logging.DEBUG)
-    logger.addHandler(fileHandler)
-elif args.autolog:
-    path = Path("logs")
-    if not path.exists():
-        logger.warn("./logs path does not exists in current folder. Creating...")
-        path.mkdir()
-    fileHandler = logging.FileHandler(
-        f'./logs/{datetime.datetime.now().strftime("%Y_%m_%d-%Hh%Mm%Ss.log")}',
-        mode="w+",
+
+if __name__ == "__main__":
+    env_list = [
+        "donkey-warehouse-v0",
+        "donkey-generated-roads-v0",
+        "donkey-avc-sparkfun-v0",
+        "donkey-generated-track-v0",
+        "donkey-roboracingleague-track-v0",
+        "donkey-waveshare-v0",
+        "donkey-minimonaco-track-v0",
+        "donkey-warren-track-v0",
+        "donkey-circuit-launch-track-v0",
+    ]
+
+    parser = argparse.ArgumentParser(
+        description="Python utility to generate data from donkey car."
     )
-    fileHandler.setLevel(logging.DEBUG)
-    logger.addHandler(fileHandler)
 
-env = setup_env()
-logger.info("Environment created, starting simulation...")
-play_simulation(env, args)
+    parser.add_argument("number", type=int, help="The number of files to generate.")
+    parser.add_argument(
+        "type",
+        choices=["straight", "turn", "random", "fixed"],
+        help="How are the tests conducted.",
+    )
+    parser.add_argument(
+        "--env",
+        choices=env_list + ["all"],
+        default="donkey-generated-track-v0",
+        help="Which map is used.",
+    )
+    parser.add_argument(
+        "--frames",
+        "-f",
+        required=False,
+        default=100,
+        type=int,
+        help="How many frames to record, better if between 100 and 10000. Default: 100.",
+    )
+    parser.add_argument(
+        "--logfile",
+        type=str,
+        required=False,
+        dest="log",
+        help="A file in which to save the logs.",
+    )
+    parser.add_argument(
+        "--autologfile",
+        action="store_true",
+        dest="autolog",
+        help="Create the logfiles automatically at a default location. Cannot be used in conjunction with --logfile.",
+    )
+    args = parser.parse_args()
+
+    if args.log:
+        if args.autolog:
+            raise AssertionError("Cannot use autologfile and logfile at the same time!")
+        fileHandler = logging.FileHandler(args.log, mode="w+")
+        fileHandler.setLevel(logging.DEBUG)
+        logger.addHandler(fileHandler)
+    elif args.autolog:
+        path = Path("logs")
+        if not path.exists():
+            logger.warn("./logs path does not exists in current folder. Creating...")
+            path.mkdir()
+        fileHandler = logging.FileHandler(
+            f'./logs/{datetime.datetime.now().strftime("%Y_%m_%d-%Hh%Mm%Ss.log")}',
+            mode="w+",
+        )
+        fileHandler.setLevel(logging.DEBUG)
+        logger.addHandler(fileHandler)
+
+    if args.env != "all":
+        env = setup_env(args.env)
+        logger.info("Environment created, starting simulation...")
+        play_simulation(env, args)
+    else:
+        for env_name in env_list:
+            env = setup_env(env_name)
+            logger.info(f"Environment {env_name} created, starting simulation...")
+            play_simulation(env, args)
